1af2cb5a4b22db2f3c69705ffafcbcbf7bf747ff
heXdzh <90160050+heXdzh@users.noreply.github.com>
2023-03-09 22:01:31 +0100

    added functionality to resize button, improved functionality of both buttons, improved grid creation by making a function to do it with instead

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+var canvasSize = prompt("Enter canvas size:", "16");
+var trueSize = canvasSize*canvasSize;
 const contGrids = document.querySelector('.contGrids');
 
-for (let i = 0; i < 256; i++) 
+function createGrid()
 {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    /*square.textContent = "Test!!!";**/
-    contGrids.appendChild(square);
-};
+    for (let i = 0; i < trueSize; i++) 
+            {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            contGrids.appendChild(square);
+            };
+}
+
+createGrid();
+
+const resizeButton = document.getElementById('btn2');
+resizeButton.addEventListener
+(
+    'click', function()
+    {
+        contGrids.innerHTML = "";
+        canvasSize = prompt("Enter canvas size:", "16");
+        trueSize = canvasSize*canvasSize;
+        createGrid();
+    }
+);
 
 
 contGrids.addEventListener
@@ -22,13 +40,16 @@ contGrids.addEventListener
 )
 
 const resetButton = document.getElementById('btn');
-
 resetButton.addEventListener
 (
     'click', function()
     {
-        location.reload();
-        return false;
+      contGrids.innerHTML = "";
+      createGrid();
     }
-);
+)
+
+
+
+
  
