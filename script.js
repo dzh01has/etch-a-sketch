23ecc6c2b518274b18f8f569ce92cf9cf6ddc65c
heXdzh <90160050+heXdzh@users.noreply.github.com>
2023-03-09 17:54:24 +0100

    added onmouseover hover effect which paints the divs, added functionality to reset button

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ for (let i = 0; i < 256; i++)
     /*square.textContent = "Test!!!";**/
     contGrids.appendChild(square);
 };
+
+
+contGrids.addEventListener
+(
+    'mouseover', e => 
+    {
+        var target = e.target
+        if (target !== contGrids)
+        {
+            target.classList.add('permahover')
+        }
+    }
+)
+
+const resetButton = document.getElementById('btn');
+
+resetButton.addEventListener
+(
+    'click', function()
+    {
+        location.reload();
+        return false;
+    }
+);
+ 
